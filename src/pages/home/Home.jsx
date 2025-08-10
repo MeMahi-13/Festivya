6abdf616bd6aef3118f2aaf117598db67419c8cf
Mohima Chowdhury <work.mohima@gmail.com>
2025-08-10 11:30:33 +0600

    font added

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -12,7 +12,7 @@ const Home = () => {
 
 
     return (
-        <div>
+        <div className='font-roboto'>
             <Banner />
             <HowItWorks />
             <Suspense fallback={'Loading Popular Jobs'}>
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+// tailwind.config.js
+export const theme = {
+    extend: {
+        fontFamily: {
+            roboto: ['Open Sans', 'sans-serif'],
+        },
+    },
+};
