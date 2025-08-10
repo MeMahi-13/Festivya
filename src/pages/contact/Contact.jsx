6190dcc05f6f5174d3d0897ccd3162625bb0ec61
Mohima Chowdhury <work.mohima@gmail.com>
2025-08-10 10:36:37 +0600

    changed bg

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,25 +1,28 @@
 const Contact = () => {
   return (
-<div className="bg-pink-50 text-black" >
-     <h1 className="text-center  bg-pink-50 text-pink-950 text-4xl font-semibold p-5">Connect with Us</h1>
+    <div className="bg-pink-50 min-h-screen flex items-center justify-center">
+      <div className="text-black mx-auto max-w-6xl p-5">
+        <h1 className="text-center text-pink-950 text-4xl font-semibold p-5">
+          Connect with Us
+        </h1>
 
         <div className="flex flex-row items-center gap-5">
-     
-      <div className="space-y-2 text-xl p-15">
-        <h2>Email:</h2>
-        <p>contact@example.com</p>
-        <p>support@example.com</p>
-        <p>info@example.com</p>
-      </div>
-      <div className="p-5 rounded-t-full">
-        <img
-          src="https://i.ibb.co/sdvDxF57/pexels-leah-newhouse-50725-540522.jpg"
-          alt="Contact Us"
-          className="w-full h-auto rounded-t-full"
-        />
+          <div className="space-y-2 text-xl p-15">
+            <h2>Email: contact@Festivya.com</h2>
+            <h2>support@example.com</h2>
+            <p>Contact: 01234567890</p>
+            
+          </div>
+          <div className="p-5 m-20 rounded-t-full">
+            <img
+              src="https://i.ibb.co/sdvDxF57/pexels-leah-newhouse-50725-540522.jpg"
+              alt="Contact Us"
+              className="rounded-t-full"
+            />
+          </div>
+        </div>
       </div>
     </div>
-</div>
   );
 };
 
